c63a870661ba0210f1d6274cd2bca4552a413a64
Vikas Pabla <107341338+vikaspabla@users.noreply.github.com>
2023-08-22 10:26:29 +0530

    Add files via upload

---
+++ Simon Game/game.js
@@ -0,0 +1,40 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
+var userClickedPattern = [];
+
+//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
+$(".btn").click(function() {
+
+  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+  var userChosenColour = $(this).attr("id");
+
+  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+  userClickedPattern.push(userChosenColour);
+
+  
+
+});
+
+
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
+  playSound(randomChosenColour);
+}
+
+//2. Create a new function called playSound() that takes a single input parameter called name.
+function playSound(name) {
+
+  //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
